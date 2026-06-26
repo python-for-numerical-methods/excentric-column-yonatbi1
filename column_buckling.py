6443da03ad14daa6493eea486cf3dce052e25b34
yonatbi1 <yonatbi1@ac.sce.ac.il>
2026-06-26 16:15:40 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -2,6 +2,17 @@ import numpy as np
 from scipy.optimize import bisect
 
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
+    """
+    L: אורך במ"מ
+    E: מודול אלסטיות ב-MPa
+    A: שטח חתך בממ"ר
+    r: רדיוס אינרציה במ"מ
+    c: מרחק לסיב קיצוני במ"מ
+    e: אקסצנטריות במ"מ
+    sigma_allow: מאמץ מותר ב-MPa
+    
+    Return: העומס P בניוטון (float)
+    """
     def f(P):
         if P <= 0:
             return -sigma_allow
